dd5a18718272113df5ece63bb7d8a2c820b1b6b2
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2021-11-07 15:04:01 +0100

    dodano: rozwiązanie zadania 1

---
+++ zad_1.py
@@ -0,0 +1,13 @@
+def loop_through_names_in_list(name_list: list):
+    for name in name_list:
+        print(name)
+
+
+name_list = list()
+name_list.append("Artur")
+name_list.append("Paweł")
+name_list.append("Arkadiusz")
+name_list.append("Jerzy")
+name_list.append("Alicja")
+
+loop_through_names_in_list(name_list=name_list)
