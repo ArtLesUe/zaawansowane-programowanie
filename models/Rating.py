c1515c9ff5d6062e782bdfd44528ca4bc3f0b750
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2021-12-05 16:08:40 +0100

    dodano: prace nad zadaniem, krok 3

---
+++ models/Rating.py
@@ -0,0 +1,6 @@
+class Rating:
+    def __init__(self, userId: int, movieId: int, rating: float, timestamp: int):
+        self.userId = userId
+        self.movieId = movieId
+        self.rating = rating
+        self.timestamp = timestamp
